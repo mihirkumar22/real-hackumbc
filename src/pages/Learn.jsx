11cd26e682e16d67873e8779a52ea6a49a0128a8
Arush <akarushkumar7@gmail.com>
2025-09-27 18:29:12 -0400

    i give up

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,13 +1,11 @@
 import React, { useRef, useEffect } from 'react'
 import CustomNavbar from '../components/CustomNavbar'
 import './Learn.css'
-import Beanstalk from '../components/Beanstalk'
 
 export default function Learn() {
     return (
         <div className="learn">
             <CustomNavbar />
-            <Beanstalk />
         </div>
     )
 }
