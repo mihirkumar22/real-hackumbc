bb3121f483743789641a9b46813c445b9a65ec88
Vsykes3 <vaibhav.sykhesh24@gmail.com>
2025-09-27 14:10:35 -0400

    Routing Structure and File structure skeleton

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import login from './pages/login'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<login />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
